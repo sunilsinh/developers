9f398ae54f0ee1c46f75707a7246608bf4683d6c
Sunil Singh <sunils3@chetu.com>
2015-12-19 17:57:18 +0530

    Dec 19 Late static binding

---
+++ late_static_binding.php
@@ -0,0 +1,20 @@
+<?php 
+
+class A {
+    public static function who() {
+        echo __CLASS__;
+    }
+    public static function test() {
+        self::who();
+        static ::who(); // late static binding 
+    }
+}
+
+class B extends A {
+    public static function who() {
+        echo __CLASS__;
+    }
+}
+
+B::test();
+
