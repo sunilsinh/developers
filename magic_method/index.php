af251afd3628e6ea04fb88daa58188ef42c1d828
Sunil Singh <sunils3@chetu.com>
2015-12-21 14:05:22 +0530

    Dec-21 add folder for magic method

---
+++ magic_method/index.php
@@ -0,0 +1,32 @@
+<?php
+error_reporting(0);
+class Customer {
+
+	public $Age;
+	public function __construct() { // automatically called when object created
+		 
+		echo "Constructor created<br />";
+	}
+	 public function __get($var){ // when unaccessible data called from object 
+ 
+    echo "Unavailable $var<br />";
+    }
+    public function __call($method,$argument){
+     echo "method is not defined <br />";
+    }
+    public function __toString() { ##It allows you to set a string value for the object that will 
+    	                           ##be used if the object is ever used as a string
+     echo "to string";
+    }
+  
+     
+
+}
+
+ $ob = new Customer(); ##Constructor created
+ $ob->email ;  ##Unavailable email
+ $ob->Age = 5;
+ $ob->display(); ##method is not defined
+ echo $ob; // to string
+ ##Method Customer::__toString() must return a string value in D:\xampp\htdocs\developers\magic_method\index.php on line 30 
+ ## after that use error_reporting();
