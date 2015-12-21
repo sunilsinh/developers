ed3564f43e23aa0277b87fa60652758acee2a639
Sunil Singh <sunils3@chetu.com>
2015-12-21 14:13:32 +0530

    Dec-21add proper comment system(doclog)

--- magic_method/index.php
+++ magic_method/index.php
@@ -1,19 +1,38 @@
 <?php
+#################Author#############
+########Sunil Singh#################
+####################################
 error_reporting(0);
 class Customer {
 
 	public $Age;
+	/*
+	* function to create magic method __constructor() 
+	* 21 Dec 2015
+	*/
 	public function __construct() { // automatically called when object created
 		 
 		echo "Constructor created<br />";
 	}
-	 public function __get($var){ // when unaccessible data called from object 
+	/*
+	* function to create magic method __get()
+	* 21 Dec 2015
+	*/
+	public function __get($var) { // when unaccessible data called from object 
  
     echo "Unavailable $var<br />";
     }
-    public function __call($method,$argument){
+    /*
+	* function to create magic method __call()
+	* 21 Dec 2015
+	*/
+    public function __call($method,$argument) {
      echo "method is not defined <br />";
     }
+    /*
+	* function to create magic method __toString() 
+	* 21 Dec 2015
+	*/
     public function __toString() { ##It allows you to set a string value for the object that will 
     	                           ##be used if the object is ever used as a string
      echo "to string";
